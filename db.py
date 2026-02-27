2c860391a0a08b5c202cdb0e2a52455fc2db38b5
Wagner Souza <wagh.cwb@gmail.com>
2026-02-26 22:06:05 -0300

    feat: adjust encoding for utf-8 based

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 
 engine = create_engine(
     "postgresql+psycopg2://postgres:98071011@localhost:5432/catalog",
-    connect_args={"options": "-c client_encoding=WIN1252"},
+    connect_args={"options": "-c client_encoding=UTF-8"},
 )
 
 with engine.connect() as connection:
